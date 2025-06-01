e6fc49db58e6044a02811c5bf7614fa3f7f79a66
Maester-Khris <ndevjoel@gmail.com>
2025-05-31 23:56:43 -0400

    bug on parallel log reading

--- djangobackend/dataapi/streaming/services.py
+++ djangobackend/dataapi/streaming/services.py
@@ -2,18 +2,41 @@ from pathlib import Path
 import os
 import json
 import random
+from concurrent.futures import ThreadPoolExecutor
 
 DATA_DIR = Path(__file__).resolve().parent.parent
-datapath = os.path.join(DATA_DIR, "data", "todos.json")
+datapath = os.path.join(DATA_DIR, "data", "logs")
+
+def read_file_in_chunk(filepath, chunk_size):
+    chunks = []
+    with open(filepath, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            chunks.append(chunk)
+    return chunks
 
 def load_data():
-    """Loads the data from the JSON file."""
-    with open(datapath, 'r') as file:
-        return json.load(file)
+    chunk_size = 1024 * 1024
+    logs_files = [filename for filename in os.listdir(datapath)]
+    logs_file_path = [os.path.join(datapath, f) for f in logs_files]
+    result = {}
+
+    #parrallel files readding
+    with ThreadPoolExecutor() as executor:
+        future_to_file = {executor.submit(read_file_in_chunk, path, chunk_size): fname for path, fname in zip(logs_file_path, logs_files)}
+
+        for future in future_to_file:
+            fname = future_to_file[future]
+            result[fname] = future.result()
+
+    return result
 
 def data_producer():
     """Chooses a random object from the loaded data."""
     data = load_data()  # Load data on each call
-    todo_user = random.choice(data) #use random.choice for better readability
+    print(data.keys())
+    todo_user = random.choice(data.values) #use random.choice for better readability
     print(f"selected todo, {todo_user}")
     return todo_user

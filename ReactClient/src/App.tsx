2e435033786cdc831519cb61379b073ba5b8216e
Maester-Khris <ndevjoel@gmail.com>
2025-06-03 12:04:29 -0400

    added initial folder for react client

---
+++ ReactClient/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import CallToActionButton from './components/CallToActionButton'
+
+
+function App() {
+  return (
+    <>     
+      <CallToActionButton />
+    </>
+  )
+}
+
+export default App;
---
+++ ReactClient/src/components/CallToActionButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function CallToActionButton() {
+
+  const handleClick = () =>{
+    console.log("yeah man!");
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <button 
+          className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700"
+          onClick = {handleClick}
+          >
+            Start monitoring
+        </button>
+    </div>
+  )
+}
+
+export default CallToActionButton;
